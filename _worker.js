c768d7a2651a211f089379bf716b6d839917ecd0
vertiljivenson9 <vertiljivenson9@gmail.com>
2025-12-31 22:30:37 -0400

    VERTIL_CORE_SYNC: Replicación Cabeza-a-Pies v10.0 [Atomic Bundle]

--- _worker.js
+++ _worker.js
@@ -3,6 +3,12 @@ export default {
     const url = new URL(request.url);
     try {
       let res = await env.ASSETS.fetch(request);
+      if (res.ok && (url.pathname.endsWith('.js') || url.pathname === '/bundle.js')) {
+        const content = await res.blob();
+        const headers = new Headers(res.headers);
+        headers.set('Content-Type', 'application/javascript');
+        return new Response(content, { headers });
+      }
       if (res.status === 404 && !url.pathname.includes('.')) {
         return await env.ASSETS.fetch(new URL('/index.html', url.origin));
       }
